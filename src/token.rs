6661e32ba8bffb27d4c1a0ab88fa116cb2f6f2c3
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-23 21:58:30 -0400

    Adding basics to tokenize simple math equations

--- src/token.rs
+++ src/token.rs
@@ -19,9 +19,34 @@ pub struct Tokenizer<'a> {
 }
 
 impl<'a> Tokenizer<'a> {
-	pub fn new(s: &str) -> Self {
+	pub fn new(s: &'a str) -> Self {
 		Tokenizer {
 			it: s.chars().peekable(),
 		}
+	}
+}
+
+fn is_digit(c: char) -> bool {
+	c >= '0' && c <= '9'
+}
+
+impl <'a> Iterator for Tokenizer<'a> {
+	// Using String error here to be lazy.
+	type Item = Result<Token, String>;
+	fn next(&mut self) -> Option<Self::Item> {
+		while let Some(c) = self.it.next() {
+			match c {
+				' ' | '\n' | '\t' => {}
+				'(' => return Some(Ok(Token::BrOpen)),
+				')' => return Some(Ok(Token::BrClose)),
+				'+' => return Some(Ok(Token::Add)),
+				'-' => return Some(Ok(Token::Sub)),
+				'/' => return Some(Ok(Token::Div)),
+				'*' => return Some(Ok(Token::Mul)),
+				v if is_digit(v) => {},
+				c => return Some(Err(format!("unexpected '{}'", c))),
+			}
+		}
+		None
 	}
 }
