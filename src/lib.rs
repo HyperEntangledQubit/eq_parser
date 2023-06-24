650722ba00b2c783b8d254124beb7ebc8ec25f76
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-23 21:36:54 -0400

    Adding stub to box expressions around operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
 mod token;
+
+use std::str::FromStr;
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Expr {
+	Brackets(Box<Expr>),
+	Op(Oper, Box<Expr>, Box<Expr>),
+	Num(i64),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Oper {
+	Add,
+	Sub,
+	Div,
+	Mul,
+}
+
+pub fn op(o: Oper, a: Expr, b: Expr) -> Expr {
+	Expr::Op(o, Box::new(a), Box::new(b))
+}
+
+impl FromStr for Expr {
+	type Err = String;
+	fn from_str(_s: &str) -> Result<Expr, String> {
+		unimplemented!{}
+	}
+}
