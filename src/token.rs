23a41be5e9e6b2da3f8993a2d12e0beaa9849f28
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-23 22:17:56 -0400

    Adding test for first cut. Surprisingly it works

--- src/token.rs
+++ src/token.rs
@@ -30,6 +30,23 @@ fn is_digit(c: char) -> bool {
 	c >= '0' && c <= '9'
 }
 
+pub fn num_token(it: &mut IT, c:char) -> i64 {
+	// Subtract 0 from char after cast as i64
+	let mut result = c as i64 - 48; // 48 here is 0 in ASCII
+	// Peek because not sure how long the number is.
+	// Could be some other operator
+	while let Some(c) = it.peek() {
+		if is_digit(*c) {
+			result = result * 10 + *c as i64 - 48;
+		} else {
+			return result;
+		}
+		it.next(); // Need this because peek wont move iterator forward
+							 // resulting in inf loop
+	}
+	result
+}
+
 impl <'a> Iterator for Tokenizer<'a> {
 	// Using String error here to be lazy.
 	type Item = Result<Token, String>;
@@ -43,10 +60,29 @@ impl <'a> Iterator for Tokenizer<'a> {
 				'-' => return Some(Ok(Token::Sub)),
 				'/' => return Some(Ok(Token::Div)),
 				'*' => return Some(Ok(Token::Mul)),
-				v if is_digit(v) => {},
+				v if is_digit(v) => return Some(Ok(Token::Num(
+							num_token(&mut self.it, v)))),
 				c => return Some(Err(format!("unexpected '{}'", c))),
 			}
 		}
 		None
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn test_tokenizer() {
+		let mut tk  = Tokenizer::new("38+12       + (34*2)");
+		assert_eq!(tk.next(), Some(Ok(Token::Num(38))));
+		assert_eq!(tk.next(), Some(Ok(Token::Add)));
+		assert_eq!(tk.next(), Some(Ok(Token::Num(12))));
+		assert_eq!(tk.next(), Some(Ok(Token::Add)));
+		assert_eq!(tk.next(), Some(Ok(Token::BrOpen)));
+		assert_eq!(tk.next(), Some(Ok(Token::Num(34))));
+		assert_eq!(tk.next(), Some(Ok(Token::Mul)));
+		assert_eq!(tk.next(), Some(Ok(Token::Num(2))));
+		assert_eq!(tk.next(), Some(Ok(Token::BrClose)));
 	}
 }
